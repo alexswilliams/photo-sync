166e95f1aa9c24a71a18bdf5875fd8d5a0b1d204
Alex Williams <lexw@live.co.uk>
2025-01-07 18:17:24 +0000

    version bumps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 // https://gradle.org/releases/
-// ./gradlew wrapper --gradle-version=8.11 --distribution-type=BIN
+// ./gradlew wrapper --gradle-version=8.12 --distribution-type=BIN
 
 plugins {
     // https://mvnrepository.com/artifact/org.jetbrains.kotlin.jvm/org.jetbrains.kotlin.jvm.gradle.plugin
-    kotlin("jvm") version "2.0.21"
+    kotlin("jvm") version "2.1.0"
     application
 }
 
@@ -16,23 +16,23 @@ repositories {
 
 dependencies {
     // https://mvnrepository.com/artifact/aws.sdk.kotlin/s3
-    val kotlinSdkVersion = "1.3.72"
+    val kotlinSdkVersion = "1.3.106"
 
     implementation("aws.sdk.kotlin:s3:$kotlinSdkVersion")
     implementation("aws.sdk.kotlin:s3control:$kotlinSdkVersion")
 
     // https://mvnrepository.com/artifact/aws.smithy.kotlin/http-client-engine-okhttp
-    implementation("aws.smithy.kotlin:http-client-engine-okhttp:1.3.21")
+    implementation("aws.smithy.kotlin:http-client-engine-okhttp:1.3.32")
 
     // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-core
-    val kotlinCoroutinesVersion = "1.9.0"
+    val kotlinCoroutinesVersion = "1.10.1"
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
 
     // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
     runtimeOnly("org.slf4j:slf4j-simple:2.0.16")
 
     // https://mvnrepository.com/artifact/io.github.oshai/kotlin-logging-jvm
-    implementation("io.github.oshai:kotlin-logging-jvm:7.0.0")
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
 }
 
 kotlin {
