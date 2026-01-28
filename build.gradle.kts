99e838fdcd4d45ee2bbc7ae6cbbff7312d99afe1
Alex Williams <lexw@live.co.uk>
2026-01-28 23:43:13 +0000

    version bumps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 // https://gradle.org/releases/
-// ./gradlew wrapper --gradle-version=9.1.0 --distribution-type=BIN  && ./gradlew wrapper
+// ./gradlew wrapper --gradle-version=9.3.0 --distribution-type=BIN  && ./gradlew wrapper
 
 plugins {
     // https://mvnrepository.com/artifact/org.jetbrains.kotlin.jvm/org.jetbrains.kotlin.jvm.gradle.plugin
-    kotlin("jvm") version "2.2.20"
+    kotlin("jvm") version "2.3.0"
     // https://github.com/graalvm/native-build-tools/releases
-    id("org.graalvm.buildtools.native") version "0.11.1"
+    id("org.graalvm.buildtools.native") version "0.11.4"
     application
 }
 
@@ -18,13 +18,13 @@ repositories {
 
 dependencies {
     // https://mvnrepository.com/artifact/aws.sdk.kotlin/s3
-    val kotlinSdkVersion = "1.5.63"
+    val kotlinSdkVersion = "1.6.4"
 
     implementation("aws.sdk.kotlin:s3:$kotlinSdkVersion")
     implementation("aws.sdk.kotlin:s3control:$kotlinSdkVersion")
 
     // https://mvnrepository.com/artifact/aws.smithy.kotlin/http-client-engine-okhttp
-    implementation("aws.smithy.kotlin:http-client-engine-okhttp:1.5.14")
+    implementation("aws.smithy.kotlin:http-client-engine-okhttp:1.6.0")
 
     // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-core
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
@@ -33,17 +33,17 @@ dependencies {
     runtimeOnly("org.slf4j:slf4j-simple:2.0.17")
 
     // https://mvnrepository.com/artifact/io.github.oshai/kotlin-logging-jvm
-    implementation("io.github.oshai:kotlin-logging-jvm:7.0.13")
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.14")
 
     // https://mvnrepository.com/artifact/org.bouncycastle/bcprov-jdk18on
-    implementation("org.bouncycastle:bcprov-jdk18on:1.82")
+    implementation("org.bouncycastle:bcprov-jdk18on:1.83")
 
     // https://mvnrepository.com/artifact/org.junit/junit-bom
-    testImplementation(platform("org.junit:junit-bom:5.14.0"))
+    testImplementation(platform("org.junit:junit-bom:6.0.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     // https://mvnrepository.com/artifact/org.assertj/assertj-core
-    testImplementation("org.assertj:assertj-core:3.27.6")
+    testImplementation("org.assertj:assertj-core:3.27.7")
 }
 
 tasks.test {
